796a8fd037c073e2b91455a13162321731dd27e3
cwaku <rickymormor1@gmail.com>
2022-06-12 17:28:18 +0000

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -21,18 +21,13 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(size.height);
     document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
   }, [size.height]);
-
-  useEffect(() => {
-    requestAnimationFrame(() => skewScroll());
-  });
 
   const skewScroll = () => {
     skewConfig.current = window.scrollY;
     skewConfig.previous
-      += (skewConfig.current - skewConfig.previous) * skewConfig.ease;
+    += (skewConfig.current - skewConfig.previous) * skewConfig.ease;
     skewConfig.rounded = Math.round(skewConfig.previous * 100) / 100;
 
     const difference = skewConfig.current - skewConfig.rounded;
@@ -45,29 +40,22 @@ function App() {
     requestAnimationFrame(() => skewScroll());
   };
 
+  useEffect(() => {
+    requestAnimationFrame(() => skewScroll());
+  });
+
   return (
     <div ref={app} className="App">
       <Navbar />
       <div ref={scrollContainer} className="scroll-container">
-        <div className="content">
-          {/* list images and texts */}
-          <p>
-            Hello, I'm a software developer based in New York, NY. I'm currently working on a project called
-            <a href="https://www.github.com/joshuacox/joshuacox.github.io">joshuacox.github.io</a>
-            .
-          </p>
-          <p>I'm also a student at the University of Washington studying Computer Science and Engineering.</p>
-          <p>I'm currently looking for a full-time position as a software developer.</p>
-          <p>I'm also a fan of the following:</p>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
     </div>
   );
 }
