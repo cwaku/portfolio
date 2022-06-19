eda9eb66c83c79e4c5b73b3525899f0e53884bb7
cwaku <rickymormor1@gmail.com>
2022-06-19 21:17:42 +0000

    🛠  WORKING ON: header introduction

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,4 @@
+import '../../stylesheets/header.css';
 const Header = () => (
   <>
     <div className="header">
@@ -6,22 +7,20 @@ const Header = () => (
           <h1>Ricky Mormor</h1>
           <h2>Full Stack Developer</h2>
         </div>
-        <div className="header-content-details">
-          <div className="header-content-details-summary">
-            <p>
-              You have a project you need coded?
-              <br />
-              Don’t hesitate to contact me.
-            </p>
-          </div>
-          <div className="header-content-details-contact">
-            <button type="button" className="header-content-details-contact-button">
-              See Projects
-            </button>
-            <button type="button" className="header-content-details-contact-button">
-              Download CV
-            </button>
-          </div>
+        <div className="header-content-summary">
+          <p>
+            You have a project you need coded?
+            <br />
+            Don’t hesitate to contact me.
+          </p>
+        </div>
+        <div className="header-content-details-contact">
+          <button type="button" className="header-content-details-contact-button">
+            See Projects
+          </button>
+          <button type="button" className="header-content-details-contact-button">
+            Download CV
+          </button>
         </div>
       </div>
     </div>
