749eed9d9428d28add973e3a056fcf1782ef7165
cwaku <rickymormor1@gmail.com>
2022-06-20 12:54:25 +0000

    👌 IMPROVE: use library instead of custom hook for typewriter animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,39 +1,53 @@
-import useTypedRole from '../../hooks/useTypedRole';
+import TypeWriterEffect from 'react-typewriter-effect';
+
 import '../../stylesheets/header.css';
 
-const roles = ['Software Engineer', 'Front-end Developer', 'Full Stack Developer'];
 
-const Header = () => {
-  const typedRole = useTypedRole(roles);
-  return (
-    <>
-      <div className="header">
-        <div className="header-content">
-          <h3 className="header-content-name">
-            Ricky
-            <span> Mormor.</span>
+const Header = () => (
+  <>
+    <div className="header">
+      <div className="header-content">
+        <h3 className="header-content-name">
+          Ricky
+          <span> Mormor.</span>
+          <br />
+          <TypeWriterEffect
+            textStyle={{
+              fontFamily: 'DM Sans',
+              color: '#fafafa',
+              fontWeight: 500,
+              fontSize: '34px',
+            }}
+            startDelay={2000}
+            pauseDelay={1000}
+            loopDelay={1000}
+            cursorColor="#3F3D56"
+            multiText={[
+              'Software Engineer',
+              'Front-end Developer',
+              'Full Stack Developer',
+            ]}
+            multiTextDelay={1000}
+            typeSpeed={30}
+          />
+        </h3>
+        <div className="header-content-summary">
+          <p>
+            You have a project you need coded?
             <br />
-            {typedRole}
-          </h3>
-          <div className="header-content-summary">
-            <p>
-              You have a project you need coded?
-              <br />
-              Don’t hesitate to contact me.
-            </p>
-          </div>
-          <div className="header-content-details-contact">
-            <button type="button" className="header-button-see-projects">
-              See Projects
-            </button>
-            <button type="button" className="header-button-cv">
-              Download CV
-            </button>
-          </div>
+            Don’t hesitate to contact me.
+          </p>
+        </div>
+        <div className="header-content-details-contact">
+          <button type="button" className="header-button-see-projects">
+            See Projects
+          </button>
+          <button type="button" className="header-button-cv">
+            Download CV
+          </button>
         </div>
       </div>
-    </>
-  );
-};
-
+    </div>
+  </>
+);
 export default Header;
