d168d310bca28785a3d6b01ceae710559adfe136
cwaku <rickymormor1@gmail.com>
2022-06-20 07:36:45 +0000

    🛠  WORKING ON: typing animation in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,39 @@
+import useTypedRole from '../../hooks/useTypedRole';
 import '../../stylesheets/header.css';
 
-const Header = () => (
-  <>
-    <div className="header">
-      <div className="header-content">
-        <h3 className="header-content-name">
-          Ricky
-          <span> Mormor.</span>
-          <br />
-          Software Engineer
-        </h3>
-        <div className="header-content-summary">
-          <p>
-            You have a project you need coded?
+const roles = ['Software Engineer', 'Front-end Developer', 'Full Stack Developer'];
+
+const Header = () => {
+  const typedRole = useTypedRole(roles);
+  return (
+    <>
+      <div className="header">
+        <div className="header-content">
+          <h3 className="header-content-name">
+            Ricky
+            <span> Mormor.</span>
             <br />
-            Don’t hesitate to contact me.
-          </p>
-        </div>
-        <div className="header-content-details-contact">
-          <button type="button" className="header-button-see-projects">
-            See Projects
-          </button>
-          <button type="button" className="header-button-cv">
-            Download CV
-          </button>
+            {typedRole}
+          </h3>
+          <div className="header-content-summary">
+            <p>
+              You have a project you need coded?
+              <br />
+              Don’t hesitate to contact me.
+            </p>
+          </div>
+          <div className="header-content-details-contact">
+            <button type="button" className="header-button-see-projects">
+              See Projects
+            </button>
+            <button type="button" className="header-button-cv">
+              Download CV
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Header;
