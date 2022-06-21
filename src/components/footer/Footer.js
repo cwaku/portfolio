9cefa672a737a999385ec5a9a376ff8c6c8447d0
cwaku <rickymormor1@gmail.com>
2022-06-21 09:53:10 +0000

    🎨 STYLE: footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,3 +1,5 @@
+import '../../stylesheets/Footer.css';
+
 const Footer = () => (
   <footer className="footer">
     <div className="footer__container">
@@ -5,26 +7,25 @@ const Footer = () => (
         <div className="footer__container__left__logo">
           <a href="/">RM</a>
         </div>
-        <div className="footer__container__left__text">
-          <p>DDD</p>
-        </div>
       </div>
-      <div className="footer__container__second">
-        <ul>
-          <li>Home</li>
-          <li>About me</li>
-          <li>Projects</li>
-          <li>Contact me</li>
-        </ul>
-      </div>
+      <ul className="footer__container__second">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact me</li>
+      </ul>
       <div className="footer__container__social">
-        <ul>
+        <p>Social</p>
+        <ul className="social__links">
           <li>LinkedIn</li>
           <li>Github</li>
           <li>Twitter</li>
           <li>Instagram</li>
         </ul>
       </div>
+    </div>
+    <div className="footer__container__copyright">
+      <p>© 2022 Ricky Mormor. All rights reserved.</p>
     </div>
   </footer>
 );
