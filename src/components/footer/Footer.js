639188cec71c997ac054c8670af3e356f602ca2a
cwaku <rickymormor1@gmail.com>
2022-06-21 09:23:45 +0000

    ✅ FEAT: add footer and style about me

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,32 @@
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer__container">
+      <div className="footer__container__left">
+        <div className="footer__container__left__logo">
+          <a href="/">RM</a>
+        </div>
+        <div className="footer__container__left__text">
+          <p>DDD</p>
+        </div>
+      </div>
+      <div className="footer__container__second">
+        <ul>
+          <li>Home</li>
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Contact me</li>
+        </ul>
+      </div>
+      <div className="footer__container__social">
+        <ul>
+          <li>LinkedIn</li>
+          <li>Github</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
