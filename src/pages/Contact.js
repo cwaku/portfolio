5f42a9ce86610a8b069bfc13b78dc5487c4ea136
cwaku <rickymormor1@gmail.com>
2022-06-26 15:48:22 +0000

    🎨 STYLE: contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,25 +4,25 @@ const Contact = () => (
   <div className="contact-container">
     <div className="contact-content">
       <h1 className="contact-content__title">¡Let’s talk!</h1>
-      <p>
+      <p className="contact-content__description">
         if you are interested in my work or have any concerns, do not hesitate to write to me
       </p>
     </div>
-    <div className="contact-form">
-      <form action="https://formspree.io/xqjqjqjq" method="POST">
-        <label className="contact-form__label" htmlFor="name">
+    <div>
+      <form className="contact-form" action="https://formspree.io/" method="POST">
+        <label className="contact-form__label contact-form__name" htmlFor="name">
           Your name
-          <input type="text" name="name" id="name" placeholder="Name" />
+          <input className="name__input" type="text" name="name" id="name" placeholder="Name" />
         </label>
-        <label className="contact-form__label" htmlFor="email">
+        <label className="contact-form__label contact-form__email" htmlFor="email">
           Your e-mail
-          <input type="email" name="email" id="email" placeholder="example@gmail.com" />
+          <input className="email__input" type="email" name="email" id="email" placeholder="example@gmail.com" />
         </label>
-        <label className="contact-form__label" htmlFor="message">
+        <label className="contact-form__label contact-form__textarea" htmlFor="message">
           Your message
-          <textarea name="message" id="message" placeholder="Hey..." />
+          <textarea className="form__textarea" name="message" id="message" placeholder="Hey..." />
         </label>
-        <input type="submit" value="Submit" />
+        <input className="contact-from__submit" type="submit" value="Submit" />
       </form>
     </div>
 
