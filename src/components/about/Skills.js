2609f9cb3ecc6a3131c5c3ee93084d3ff28774d8
Ricky Mormor <rickymormor1@gmail.com>
2022-07-15 14:00:14 +0000

    👌 IMPROVE: update description on about page

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -7,11 +7,13 @@ const Skills = () => (
     <div className="skills__container__skills">
       <div className="skills__container__skills__skill">
         <img src={require('../../assets/images/device-mobile.png')} alt="mobile-device" className="skills__container__skills__skill__image" />
-        <h2 className="skills__container__skills__skill__title">Responsive design</h2>
+        <h2 className="skills__container__skills__skill__title">Code review</h2>
         <div className="skills__container__skills__skill__description">
           <p>
-            With my experience in responsive design patterns I have a clear vision about
-            how to implement them effectively in any project.
+            With my experience as a Technical Support Engineer, I have the ability to
+            review and test code to ensure that it is maintainable, free of bugs,
+            and scalable. I also verify that the code is correct and effective for the requirements
+            at hand.
           </p>
         </div>
       </div>
@@ -31,17 +33,18 @@ const Skills = () => (
         <div className="skills__container__skills__skill__description">
           <p>
             I can maintain communication with the development team, attentive to any concern that
-            may arise in order to carry out the project correctly
+            may arise in order to carry out the project correctly.
           </p>
         </div>
       </div>
       <div className="skills__container__skills__skill">
         <img src={require('../../assets/images/Icon (1).png')} alt="Icon(1)" className="skills__container__skills__skill__image" />
-        <h2 className="skills__container__skills__skill__title">Design system</h2>
+        <h2 className="skills__container__skills__skill__title">Remote pair-programming</h2>
         <div className="skills__container__skills__skill__description">
           <p>
-            I have management of design systems, which allows agility in the design process,
-            shortening the time in the creation of interfaces within the project
+            I have developed skills in remote pair-programming using GitHub,
+            industry-standard git-flow,
+            and daily standup to communicate and collaborate with international remote developers.
           </p>
         </div>
       </div>
@@ -51,18 +54,18 @@ const Skills = () => (
         <div className="skills__container__skills__skill__description">
           <p>
             I am able to iterate receiving and interpreting the feedback that is provided
-            to me in order to improve the quality of the product that I am designing
+            to me in order to improve the quality of the product that I am developing.
           </p>
         </div>
       </div>
       <div className="skills__container__skills__skill">
         <img src={require('../../assets/images/code.png')} alt="code" className="skills__container__skills__skill__image" />
-        <h2 className="skills__container__skills__skill__title">HTML</h2>
+        <h2 className="skills__container__skills__skill__title">Full-stack Web Development</h2>
         <div className="skills__container__skills__skill__description">
           <p>
-            I have a strong background in HTML5 and CSS3.
-            I have a strong background in HTML5 and CSS3.
-            I have a strong background in HTML5 and CSS3.
+            I spent the last several months mastering algorithms,
+            data structures, and full-stack development while simultaneously
+            developing projects with Ruby, Rails, JavaScript, React, and Redux.
           </p>
         </div>
       </div>
