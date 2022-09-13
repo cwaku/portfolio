de4ceca037a3a3d0a7d89dcd4a787397ea3952dd
Ricky Mormor <rickymormor1@gmail.com>
2022-09-13 20:15:25 +0000

    👌 IMPROVE: update about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,15 +8,13 @@ const AboutMe = () => (
       <div className="about-me__container__text">
         <h1 className="about-me__container__text__title">About Me</h1>
         <p className="about-me__container__text__description">
-          A highly motivated Full-stack Engineer and Blockchain enthusiast
-          who likes to challenge himself with the latest trends.
-          I am looking for companies that value creativity, hard work, and leadership.
-          I am dedicated to creating and properly maintaining web apps with a passion
-          for creative solutions. While it all started as an interest in code, later on,
-          I found myself E-learning and studying by myself through online courses until
-          I enrolled in Microverse Program.
-          I am
-          currently working as a technical support engineer at
+          Logical and results-driven Full Stack developer with a strong love for Front
+          End. Dedicated to building user-focused applications using javascript,
+          Typescript, Ruby, Ruby on Rails, ReactJS & Next.js. Judicious and
+          creative when crafting effective solutions to propel competitive
+          advantage and revenue growth under client&apos;s requirements. Analytical
+          problem solver with a calm and focused demeanor and open to receiving
+          feedback. I am currently working part-time as a technical support engineer at
           {' '}
           <a
             href="https://www.linkedin.com/school/microverseinc/mycompany/"
@@ -27,10 +25,11 @@ const AboutMe = () => (
           </a>
           .
         </p>
-        <button type="button" className="about-me__container__text__button header-button">
-          <Link to="/about">
-            More about me
-          </Link>
+        <button
+          type="button"
+          className="about-me__container__text__button header-button"
+        >
+          <Link to="/about">More about me</Link>
         </button>
       </div>
       <div className="about-me__container__image">
--- src/pages/About.js
+++ src/pages/About.js
@@ -11,22 +11,35 @@ const About = () => (
       <div className="about__container__text">
         <h1 className="about__container__text__title">Ricky Mormor</h1>
         <p className="about__container__text__description">
-          I&apos;m a Full-stack Engineer who presently lives in Accra,
-          the capital and largest city of Ghana, on the Atlantic coast of West Africa.
+          I&apos;m a Full-stack Developer who presently lives in Accra, the
+          capital and largest city of Ghana, on the Atlantic coast of West
+          Africa.
         </p>
         <p className="about__container__text__description">
-          I like the idea of working as a team and think it is a way to connect new skills,
-          offer my knowledge to the team and improve productivity in developing a creative solution.
-          I like to discover new things daily and be a valuable asset to teams I collaborate with,
-          so I am constantly learning and researching current trends. I seek as much feedback as
-          possible on projects to improve and provide solutions that are not only aesthetically
-          attractive but also accessible and functional, which is why I like to work closely
-          with the stakeholders.
+          I like the idea of working as a team and think it is a way to connect
+          new skills, offer my knowledge to the team and improve productivity in
+          developing a creative solution. I like to discover new things daily
+          and be a valuable asset to teams I collaborate with, so I am
+          constantly learning and researching current trends. I seek as much
+          feedback as possible on projects to improve and provide solutions that
+          are not only aesthetically attractive but also accessible and
+          functional, which is why I like to work closely with the stakeholders.
         </p>
-        <button type="button" className="about__container__text__button header-button">
-          <Link to="/contact">
-            Let&apos;s talk!
-          </Link>
+        <p className="about__container__text__description">
+          My track record includes successes in leadership roles at two
+          different companies. At one company, I took on the frontend team
+          leadership position, while at the second company, I led and
+          shepherded the company&apos;s front end using JavaScript and React. One
+          of my main accomplishments during that time was my ability to switch
+          roles to work with environments that utilized Ruby on Rail, Ruby,
+          TypeScript and Next.js, which sharpened my skills in these
+          technologies.
+        </p>
+        <button
+          type="button"
+          className="about__container__text__button header-button"
+        >
+          <Link to="/contact">Let&apos;s talk!</Link>
         </button>
       </div>
       <div className="about__container__image">
@@ -39,8 +52,8 @@ const About = () => (
         />
       </div>
     </div>
-    <Skills />
     <TechSTack />
+    <Skills />
   </div>
 );
 
