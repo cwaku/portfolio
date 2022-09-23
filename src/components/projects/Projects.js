943dcb46d0e9ce895402a86b7d3a5b727e6af198
Ricky Mormor <rickymormor1@gmail.com>
2022-09-23 19:43:41 +0000

    🛠  WORKING ON: hiding keys

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Octokit } from '@octokit/core';
 
 const Projects = () => {
+  const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
+  useEffect(() => {
+    octokit.request('GET /user/repos', {
+      username: 'cwaku',
+      type: 'owner',
+      sort: 'updated',
+      direction: 'desc',
+      per_page: 100,
+    }).then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div>Projects</div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
