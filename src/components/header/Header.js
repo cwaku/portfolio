080a4ac2759a4397fa5c2d36be4b12f932bc120f
Ricky Mormor <rickymormor1@gmail.com>
2022-09-25 17:31:11 +0000

    📦 CHORE: update link to CV

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -102,7 +102,7 @@ const Header = () => (
             className="header-button-see-projects header-button"
           >
             <a
-              href="https://drive.google.com/file/d/1uRal6I-inPMM3MmwC2RH_UHft1RYaEH0/view?usp=sharing"
+              href="https://drive.google.com/file/d/10_4gs7KFJfxL7cB0Y6aa2NgKXD4wMvgO/view?usp=sharing"
               target="_blank"
               download
               rel="noreferrer"
